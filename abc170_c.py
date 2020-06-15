bc4142492320f493fe48937870fda8eff64c52e1
jamjamjam <50306919+jamjamjam888@users.noreply.github.com>
2020-06-16 00:56:42 +0900

    Delete abc170_c.py

--- abc170_c.py
+++
@@ -1,8 +0,0 @@
-X, N = map(int, input().split())
-p = list(map(int, input().split()))
- 
-ans = 0
-for i in range(1, 102):
-    if abs(X - i) < abs(X - ans) and i not in p:
-        ans = i
-print(ans)
